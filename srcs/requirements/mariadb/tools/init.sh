fd52486ae78cbc9e16e985141c9669242c1f9f7b
OB4413 <lionousama4@gmail.com>
2026-02-22 10:50:47 -0500

    i fixed the problem in mariadb

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -1,18 +1,26 @@
-#!/bin/bash
+#!/bin/sh
 
-mysqld_safe &
+mkdir -p /run/mysqld
+chown -R mysql:mysql /run/mysqld
 
-while ! mysqladmin ping --silent; do
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+    mariadb-install-db --user=mysql --datadir=/var/lib/mysql
+fi
+
+mysqld --skip-networking --user=mysql &
+    
+until mysqladmin ping --silent; do
     sleep 1
 done
+
 mysql -u root <<EOF
-CREATE DATABASE IF NOT EXISTS \${MYSQL_DATABASE}\;
+CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
 CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
-GRANT ALL PRIVILEGES ON \${MYSQL_DATABASE}\.* TO '${MYSQL_USER}'@'%';
+GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
 FLUSH PRIVILEGES;
 EOF
 
-mysqladmin -u root -p${MYSQL_ROOT_PASSWORD} shutdown
+mysqladmin -u root -p"${MYSQL_ROOT_PASSWORD}" shutdown
 
-exec mysqld_safe
+exec mysqld --user=mysql
